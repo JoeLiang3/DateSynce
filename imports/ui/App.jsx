d8aa31d14ce477820dbfdeab9692095bf0f6255d
Joe <joe.liang36@gmail.com>
2017-08-28 23:11:06 -0700

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,59 @@
+import React, { Component, Proptypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data'; // To use react meteor data
+
+
+import Calendar from '../components/generate_date.js'; // Date Generate button
+import FreeDays from '../components/test.jsx'; // Date Generate button
+
+import { Tasks } from '../api/tasks.js';
+
+
+
+
+
+// App component - represents the whole app
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.remove = this.remove.bind(this);
+    //Setting up all days as free dates
+    var Calendar = new Array(32);
+    for(var i=0; i<32; i++) {
+      Calendar[i] = i;
+    }
+    this.state = {
+      Calendar: Calendar,
+    };
+
+  }
+
+  // rendarMonth () {
+  //   return this.props.Adates.map((date) => (
+  //     <h1>date</h1>
+  //   ));
+  // }
+
+  remove(date) {
+    var tempCalendar = this.state.Calendar;
+    console.log(date);
+    tempCalendar[date] = 0;
+    this.setState({Calendar: tempCalendar});
+  }
+
+  render() {
+    var date = new Date();
+    var d= new Date(date.getFullYear(), date.getMonth()+1, 0);
+    var availDates = new Array(32);
+
+    return (
+
+      <div className="container">
+        <FreeDays days = {this.state.Calendar}/>
+        <Calendar remove = {this.remove}/>
+
+      </div>
+
+    );
+  }
+}
